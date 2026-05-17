a53892d7b8b2caacd39de0940e7cdd98f9409a59
goncalogribeiro <goncalo_granemann@live.com>
2026-05-17 11:59:43 -0300

    refactor: integrar dominancia dinamica ao simulador temporal

--- inventario-florestal-core/src/inventario_florestal/testing/temporal_simulator.py
+++ inventario-florestal-core/src/inventario_florestal/testing/temporal_simulator.py
@@ -7,6 +7,15 @@ from inventario_florestal.core.biological_constraints import (
     validar_crescimento_dap,
     validar_mortalidade,
 )
+from inventario_florestal.ecology.dynamic_dominance import (
+    CODOMINANTE,
+    DOMINADA,
+    DOMINANTE,
+    SUPRIMIDA,
+    classificar_posicao_sociologica,
+    dap_medio,
+    altura_media,
+)
 from inventario_florestal.ecology.temporal_competition import (
     fator_aumento_mortalidade,
     fator_reducao_crescimento,
@@ -18,6 +27,22 @@ from inventario_florestal.silviculture.silvicultural_profiles import (
 from inventario_florestal.testing.synthetic_dataset_generator import (
     ArvoreSintetica,
 )
+
+
+DOMINANCIA_CRESCIMENTO = {
+    DOMINANTE: 1.15,
+    CODOMINANTE: 1.0,
+    DOMINADA: 0.80,
+    SUPRIMIDA: 0.55,
+}
+
+
+DOMINANCIA_MORTALIDADE = {
+    DOMINANTE: 0.75,
+    CODOMINANTE: 1.0,
+    DOMINADA: 1.35,
+    SUPRIMIDA: 1.80,
+}
 
 
 
@@ -58,9 +83,23 @@ def simular_ano(
         perfil_nome
     )
 
+    dap_medio_povoamento = dap_medio(
+        arvores
+    )
+
+    altura_media_povoamento = altura_media(
+        arvores
+    )
+
     sobreviventes = []
 
     for arvore in arvores:
+        classe_sociologica = classificar_posicao_sociologica(
+            arvore,
+            dap_medio_povoamento,
+            altura_media_povoamento,
+        )
+
         indice_competicao = indice_competicao_hegyi(
             arvore,
             arvores,
@@ -74,9 +113,18 @@ def simular_ano(
             indice_competicao
         )
 
+        fator_crescimento = DOMINANCIA_CRESCIMENTO[
+            classe_sociologica
+        ]
+
+        fator_mortalidade = DOMINANCIA_MORTALIDADE[
+            classe_sociologica
+        ]
+
         mortalidade = (
             perfil.mortalidade_anual_esperada
             * aumento_mortalidade
+            * fator_mortalidade
         )
 
         if simular_mortalidade(
@@ -87,9 +135,14 @@ def simular_ano(
         incremento_dap = (
             perfil.incremento_dap_esperado_cm_ano
             * reducao
+            * fator_crescimento
         )
 
-        incremento_altura = 0.8 * reducao
+        incremento_altura = (
+            0.8
+            * reducao
+            * fator_crescimento
+        )
 
         arvore_atualizada = simular_crescimento_anual(
             arvore,
